b6854d2ebd097aecf27c90663e64356eb00f3a38
Farhan <farhansghazi@outlook.com>
2019-04-04 15:35:35 +1100

    updated .h file with comments

--- Graph.h
+++ Graph.h
@@ -10,14 +10,32 @@ typedef struct  GraphRep    *Graph;
 typedef struct _adjListNode *AdjList;
 typedef struct  EdgeRep     *Edge;
 
-Edge    newEdge         (int source   , int dest , int weight);
-Graph   newGraph        (Edge * edges , int no_of_edges);							
-AdjList newAdjListNode  (int vertex   , int weight);
 
+// Create graph by using data from egdes array
+Graph newGraph  (Edge * edges, int no_of_edges);
+
+// Allocate a new Edge structure
+Edge newEdge    (Vertex source, Vertex dest, int weight);
+
+// Allocate a new Node structure						
+AdjList newAdjListNode  (Vertex vertex, int weight);
+
+// Insert an edge between given vertices
 void  InsertEdge   (Graph g, Vertex src, Vertex dest, int weight);  // DONE
+
+//Remove an edge between given vertices
 void  RemoveEdge   (Graph g, Vertex src, Vertex dest);				// KINDA DONE
+
+// Free all the memory associated with the graph g 
+void  FreeGraph    (Graph g);
+
+// Display graph structure
+void  showGraph    (Graph g);
+
+// Determine if given vertices are adjacent to each other
 bool  Adjacent     (Graph g, Vertex src, Vertex dest);				
 
+// Parse data from given input file
 int *  ReadFile   (char * filename);
 
 // Various helper functions to test pass-around of data
@@ -27,9 +45,10 @@ int    EdgeSource (Edge e);
 int    EdgeDest   (Edge e);
 int    EdgeWeight (Edge e);
 
+// Free memory associated with Edges array
 void FreeEdgesArray(Edge * edges, int NEdges);
 
-
+// Return connections list of graph
 //AdjList * GetConnectionsArray (Graph g);
 
 
@@ -60,12 +79,5 @@ AdjList inIncident(Graph g, Vertex v);
 * Again, our tests will not use your show function, it's there for your use.
 * 
 * */
-void  showGraph(Graph g);
 
-
-/**
- * Free all the memory associated with the graph g 
- **/
-
-void  FreeGraph(Graph g);
 
