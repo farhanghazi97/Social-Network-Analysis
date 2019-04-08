ade2f3013a484d9088a3527fc01082efc19b2c08
Farhan <farhansghazi@outlook.com>
2019-04-08 14:54:21 +1000

    added comments

--- task_2/testPQ.c
+++ task_2/testPQ.c
@@ -17,12 +17,16 @@ int main(void) {
 	showPQ(new_PQ);
 	
 	// NOTE: Does not update head of list (instead appends it)
+	// TODO: This should become the head of the list as priority 
+	//       is highest (lowest weight)
 	test_item.key = 4;
 	test_item.value = 1;
 	addPQ(new_PQ , test_item);
 	showPQ(new_PQ);
 	
 	// NOTE: Does not update existing key-value pair (instead appends it)
+	// TODO: This should instead update the existing node with new value
+	
 	test_item.key = 3;
 	test_item.value = 9;
 	addPQ(new_PQ , test_item);
