e782289f9ede2428781c6eb7064ea8baa83d9eb7
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-04-17 16:50:27 +1000

    This is where we stop at on Wed

--- Ass2_Testing/LanceWilliamsHAC.c
+++ Ass2_Testing/LanceWilliamsHAC.c
@@ -10,11 +10,10 @@
 #include "Graph.h"
 #define numVertices numVerticies
 
-typedef struct Cluster {
-
-} Cluster;
 
 static double ** InitializeDistArray(Graph g);
+static Dendrogram MakeDNode (Vertex v);
+static void PrintDendArray (Dendrogram * D , int size);
 
 /*
  * Finds Dendrogram using Lance-Williams algorithm (as discussed in the specs)
@@ -28,14 +27,48 @@ static double ** InitializeDistArray(Graph g);
 Dendrogram LanceWilliamsHAC(Graph g, int method) {
 
     double ** dist_array = InitializeDistArray(g);
-
-    // Pretty print array
+    
+    Dendrogram * List = malloc(numVertices(g) * sizeof(DNode));
     for(int i = 0; i < numVertices(g); i++) {
+        List[i] = MakeDNode(i);
+    }
+    
+    PrintDendArray(List , numVertices(g));
+
+    free(List[1]);
+
+    PrintDendArray(List , numVertices(g));
+    
+    Dendrogram * temp_list = realloc(List , (numVertices(g) - 2)*sizeof(DNode) );
+    List = temp_list;
+    PrintDendArray(List, numVerticies(g));
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Pretty print array
+    /*for(int i = 0; i < numVertices(g); i++) {
         for(int j = 0; j < numVertices(g); j++) {
             printf("%.3lf\t" , dist_array[i][j]);
         }
         printf("\n");
-    }
+    }*/
 
     // Free array
     free(dist_array);
@@ -71,6 +104,20 @@ static double ** InitializeDistArray(Graph g) {
         }
     }
     return dist;
+}
+
+static Dendrogram MakeDNode (Vertex v) {
+    Dendrogram new_DNode = malloc(sizeof(struct DNode));
+    new_DNode->vertex = v;
+    new_DNode->left = NULL;
+    new_DNode->right = NULL;
+    return new_DNode;
+}
+
+static void PrintDendArray (Dendrogram * D , int size) {
+    for(int i = 0; i < size; i++) {
+        printf("VERTEX : %d\n" , D[i]->vertex);
+    }
 }
 
 void freeDendrogram(Dendrogram d) {
