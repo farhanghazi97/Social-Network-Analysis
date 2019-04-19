3d8fca448ef5a2933fad924a4d62b31a6d78007b
Kanadech J <kanadech.king@gmail.com>
2019-04-19 19:37:40 +1000

    Implemented some shit

--- Ass2_Testing/LanceWilliamsHAC.c
+++ Ass2_Testing/LanceWilliamsHAC.c
@@ -15,7 +15,8 @@ static double ** InitializeDistArray(Graph g);
 static Dendrogram MakeDNode (Vertex v);
 static void PrintDendArray (Dendrogram * D , int size);
 static void PrintDistArray (double ** dist_array , int size);
-
+static Dendrogram MakeEmptyDNode ();
+static double ** MakeNewDistArray(int size);
 /*
  * Finds Dendrogram using Lance-Williams algorithm (as discussed in the specs)
    for the given graph g and the specified method for agglomerative clustering.
@@ -28,18 +29,129 @@ static void PrintDistArray (double ** dist_array , int size);
 Dendrogram LanceWilliamsHAC(Graph g, int method) {
 
     double ** dist_array = InitializeDistArray(g);
-    
-    Dendrogram * List = malloc(numVertices(g) * sizeof(DNode));
-    for(int i = 0; i < numVertices(g); i++) {
-        List[i] = MakeDNode(i);
+    int N = numVertices(g);
+    Dendrogram * dendA = malloc(N * sizeof(DNode));
+    for(int i = 0; i < N; i++) {
+        dendA[i] = MakeDNode(i);
     }
     
-    PrintDistArray(dist_array , numVertices(g));
-    PrintDendArray(List , numVertices(g));
-
+    PrintDistArray(dist_array , N);
+    PrintDendArray(dendA , N);
+    int matSize = N;
+    for(int s = 1; s < N-1; s++){
+        // Find closest clusters and grab those indices
+        float minimum = INFINITY;
+        int index1 = 0;
+        int index2 = 0; 
+        for(int i = 0; i < matSize;i++){
+            for(int j = i; j < matSize;j++){
+                if(dist_array[j][i] < minimum){
+                    //grabbing these indices
+                    minimum = dist_array[j][i];
+                    index1 = j;
+                    index2 = i;
+                }
+            }
+        }
+        // Backup numbers, just in case I need them
+        //int backup1 = index1;
+       // int backup2 = index2;
+        // By now we would know which clusters to merge
+        Dendrogram newCluster = MakeEmptyDNode();
+        newCluster->left = dendA[index1];
+        newCluster->right = dendA[index2];
+        // Need to remove items/clean the dendA
+        if(index1>index2){
+            // I want index1 to be less than index2
+            int temp = index1;
+            index1 = index2;
+            index2 = temp;
+        }
+        // Begin resizing dendA
+        dendA[index1] = newCluster;
+        dendA[index2] = NULL;
+        for(int i = 0; i < matSize;i++){
+            //Wanna keep all cells before index1 intact only moving cells after
+            if(i>index1){
+                if (i==matSize-1) {
+                    dendA[i] = NULL;
+                } else {
+                    dendA[i] = dendA[i+1];
+                }
+            }
+        }
+        //By now we would have the new dendA array
+        //Create new dist array and perform calculation
+        double ** updatedDist = MakeNewDistArray(matSize - 1);
+        for(int i = 0; i < matSize-1;i++){
+            
+            for(int j = i; j < matSize-1;j++){
+                if (j == i){
+                    continue;
+                }
+                if(j == index1){ //index1 guarantee to be less than index 2 cuz' we swapped them
+                    // Updating horizonally using Lance Will
+                    double num1 = dist_array[index1][i];
+                    double num2 = dist_array[index2][i];
+                    if(!(num1 < INFINITY)){ num1 = dist_array[i][index1];}
+                    if(!(num2 < INFINITY)){ num2 = dist_array[i][index2];}
+                    // if(index2 > i && index1 > i){
+                    //     num1 = dist_array[index1][i];
+                    //     num2 = dist_array[index2][i];
+                    // } else if (i > index2 && i < index1){
+                    //     num1 = dist_array[i][index2];
+                    //     num2 = dist_array[index1][i];
+                    // } else if ()
+                    // if(index1>i){
+                    //     if(dist_array[index1][i] < dist_array[index2][i]){
+                    //         updatedDist[j][i] = dist_array[index1][i];
+                    //     } else {
+                    //         updatedDist[j][i] = dist_array[index2][i];
+                    //     }
+                    // }
+                    if (num1<num2){
+                        updatedDist[j][i] = num1;
+                    } else {
+                        updatedDist[j][i] = num2;
+                    }
+                } else if (i == index1) {
+                    // Updating vertically using Lance Will
+                    double num1 = dist_array[index1][j];
+                    double num2 = dist_array[index2][j];
+                    if(!(num1 < INFINITY)){ num1 = dist_array[j][index1];}
+                    if(!(num2 < INFINITY)){ num2 = dist_array[j][index2];}
+                    if (num1<num2){
+                        updatedDist[j][i] = num1;
+                    } else {
+                        updatedDist[j][i] = num2;
+                    }
+                } else {
+                    // might need more case here for vertical and horizontal
+                    if(j < index2 && i < index2){
+                        updatedDist[j][i] = dist_array[j][i];
+                    } else if (j >= index2 && i < index2) {
+                        updatedDist[j][i] = dist_array[j+1][i];
+                    } else if (j < index2 && i >= index2) {
+                        updatedDist[j][i] = dist_array[j][i+1];
+                    } else {
+                        updatedDist[j][i] = dist_array[j+1][i+1];
+                    }
+                }
+                
+            }
+        }
+        //PrintDistArray(dist_array , N);
+       // PrintDendArray(dendA , N);
+        double ** temp = dist_array;
+        dist_array = updatedDist;
+        free(temp);
+        matSize--;
+        PrintDistArray(dist_array , matSize);
+    }
+    
     // Free array
     free(dist_array);
-    return NULL;
+    return dendA[2];
 }
 
 static double ** InitializeDistArray(Graph g) {
@@ -81,6 +193,13 @@ static Dendrogram MakeDNode (Vertex v) {
     return new_DNode;
 }
 
+static Dendrogram MakeEmptyDNode () {
+    Dendrogram new_DNode = malloc(sizeof(struct DNode));
+    new_DNode->left = NULL;
+    new_DNode->right = NULL;
+    return new_DNode;
+}
+
 static void PrintDendArray (Dendrogram * D , int size) {
     for(int i = 0; i < size; i++) {
         printf("VERTEX : %d\n" , D[i]->vertex);
@@ -99,4 +218,20 @@ static void PrintDistArray (double ** dist_array , int size) {
 
 void freeDendrogram(Dendrogram d) {
 	return;
+}
+
+static double ** MakeNewDistArray(int size) {
+    // Create 2D array
+    double ** dist = calloc(size, sizeof(double *));
+    for(int i = 0; i < size; i++) {
+        dist[i] = calloc(size , sizeof(double));
+    }
+
+    // Initialize array to IFINITY values
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            dist[i][j] = INFINITY;
+        }
+    }
+    return dist;
 }
